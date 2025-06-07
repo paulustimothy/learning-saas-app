1b181d29203a846f04105adc2a4fd47428739121
paulustimothy <paulustimothy011@gmail.com>
2025-06-07 21:14:30 +0700

    Enhance companion session with new CompanionComponent and integrate Lottie animations. Update next.config for image handling and add lottie-react dependency.

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CompanionComponent from "@/components/CompanionComponent";
 import { getCompanion } from "@/lib/action/companion.actions";
 import { getSubjectColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
@@ -10,8 +11,10 @@ interface CompanionSessionPageProps {
 
 const companionSession = async ({ params }: CompanionSessionPageProps) => {
   const { id } = await params;
-  const { name, subject, topic, duration } = await getCompanion(id);
+  const companion = await getCompanion(id);
   const user = await currentUser();
+
+  const { name, subject, topic, duration } = companion;
 
   if (!user) redirect("/sign-in");
   if (!name) redirect("/companions");
@@ -44,6 +47,12 @@ const companionSession = async ({ params }: CompanionSessionPageProps) => {
           {duration} minutes
         </div>
       </article>
+      <CompanionComponent
+        {...companion}
+        companionId={id}
+        userName={user.firstName!}
+        userImage={user.imageUrl!}
+      />
     </main>
   );
 };
