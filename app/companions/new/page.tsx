f0dcafa26db40dff731caeeb06f26fbe98c8ab6f
paulustimothy <paulustimothy011@gmail.com>
2025-06-08 10:09:00 +0700

    Implement companion creation limit check and enhance subscription prompt. Update companion page to display a message when the user reaches their companion creation limit, including an upgrade option.

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -2,18 +2,47 @@ import React from "react";
 import CompanionForm from "@/components/CompanionForm";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { newCompanionPermissions } from "@/lib/action/companion.actions";
+import Image from "next/image";
+import Link from "next/link";
 
 const newCompanion = async () => {
   const { userId } = await auth();
   if (!userId) redirect("/sign-in");
 
+  const canCreateCompanion = await newCompanionPermissions();
+
   return (
     <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-      <article className="w-full gap-4 flex flex-col">
-        <h1>Companion Builder</h1>
+      {canCreateCompanion ? (
+        <article className="w-full gap-4 flex flex-col">
+          <h1>Companion Builder</h1>
 
-        <CompanionForm />
-      </article>
+          <CompanionForm />
+        </article>
+      ) : (
+        <article className="companion-limit">
+          <Image
+            src={"/images/limit.svg"}
+            alt="limit"
+            width={360}
+            height={230}
+          />
+          <div className="cta-badge">Upgrade your plan</div>
+          <h1>You&apos;ve Reached Your Limit</h1>
+          <p>
+            You&apos;ve reached the maximum number of companions you can create
+            with your current plan. Upgrade to a higher plan to create more
+            companions.
+          </p>
+          <Link
+            href="/subscription"
+            className="btn-primary w-full justify-center"
+          >
+            Upgrade My Plan
+          </Link>
+        </article>
+      )}
     </main>
   );
 };
